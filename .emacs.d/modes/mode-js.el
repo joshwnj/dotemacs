5be20e4588c35180995ce116db9db8c919f2b9e3
Josh Johnston <josh@x-team.com>
2012-03-08 17:39:02 -0800

    using javascript-mode instead of js2-mode + added flymake-jslint

--- .emacs.d/modes/mode-js.el
+++ .emacs.d/modes/mode-js.el
@@ -1,8 +1,13 @@
 ;;;;
 ;; javascript
 
-(autoload 'js2-mode "js2-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(setq js2-basic-offset 4)
+(add-to-list 'auto-mode-alist '("\\.js$" . javascript-mode))
+
+;; NOTE: expects jslint command to be in your path
+;; npm install -g jslint
+
+(require 'flymake-jslint)
+(add-hook 'javascript-mode-hook
+	  (lambda () (flymake-mode t)))
 
 (provide 'mode-js)
