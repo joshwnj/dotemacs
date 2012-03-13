bac99d6a487d734b527c5135308676c751eaaec5
Josh Johnston <josh@x-team.com>
2012-03-13 07:17:05 -0700

    buffer switching keybindings

--- .emacs.d/init/init-nav-fs.el
+++ .emacs.d/init/init-nav-fs.el
@@ -8,4 +8,12 @@
 (define-key osx-key-mode-map (kbd "A-o") 'find-file)
 (define-key ido-common-completion-map (kbd "M-/") 'ido-select-text)
 
+;;;;
+;; buffers
+
+(global-set-key (kbd "A-b") 'ido-switch-buffer)
+
+(global-set-key (kbd "A-M-]") 'previous-buffer)
+(global-set-key (kbd "A-M-[") 'next-buffer)
+
 (provide 'init-nav-fs)
