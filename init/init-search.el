89e6ac865f02cad327e20e72002f7d4d1091ada3
Josh Johnston <josh@x-team.com>
2012-03-13 16:54:08 -0700

    added color-occur

--- init/init-search.el
+++ init/init-search.el
@@ -4,6 +4,9 @@
 (setq grep-find-command
       "export GREP_OPTIONS=\"--exclude=*.svn*\"; find . -type f -print0 | xargs -0 grep -nH --color -e ")
 
+;; colors for occur-mode
+(require 'color-occur)
+
 (global-set-key (kbd "M-A-f") 'grep-find)
 (global-set-key (kbd "M-A-,") 'previous-error)
 (global-set-key (kbd "M-A-.") 'next-error)
