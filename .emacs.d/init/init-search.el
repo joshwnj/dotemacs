c991205442a0a39b609872efeace29ea017a824b
Josh Johnston <josh@x-team.com>
2012-03-13 07:17:53 -0700

    updated grep command + added keys for next/prev grep result

--- .emacs.d/init/init-search.el
+++ .emacs.d/init/init-search.el
@@ -1,7 +1,12 @@
 ;;;;
 ;; searching
 
-(setq grep-find-command 
-  "find . -type f -exec grep -nH --exclude=\"*\.svn*\" -e   {} /dev/null \\;")
+(setq grep-find-command
+      "export GREP_OPTIONS=\"--exclude=*.svn*\"; find . -type f -print0 | xargs -0 grep -nH --color -e ")
+
+(global-set-key (kbd "M-A-f") 'grep-find)
+(global-set-key (kbd "M-A-,") 'previous-error)
+(global-set-key (kbd "M-A-.") 'next-error)
+
 
 (provide 'init-search)
