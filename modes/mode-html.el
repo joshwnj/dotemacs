88eb428d1c6cd37de06b9aa0e11724905c4a8e0a
Josh Johnston <josh@x-team.com>
2012-03-14 09:22:47 -0700

    fixed mode assignment so we always get html-mode instead of html-helper-mode + added indent-region-as

--- modes/mode-html.el
+++ modes/mode-html.el
@@ -1,6 +1,27 @@
 ;;;;
 ;; html editing
 
-(add-to-list 'auto-mode-alist '("\\.html" . html-mode))
+(setq auto-mode-alist
+	  (append '(("\\.html$" . html-mode)
+                ("\\.htm$" . html-mode)
+                ("\\.tpl$" . html-mode)
+                ("\\.phtml$" . html-mode)
+                ) auto-mode-alist))
+
+;; disable the magic list (so we don't get html-helper-mode)
+(setq magic-mode-alist ())
+
+;; indenting
+(defun indent-region-as (other-mode)
+  "Indent selected region as some other mode.  Used in order to indent source code contained within HTML."
+  (interactive "aMode to use: ")
+
+  (save-excursion
+    (let ((old-mode major-mode))
+      (narrow-to-region (region-beginning) (region-end))
+      (funcall other-mode)
+      (indent-region (region-beginning) (region-end) nil)
+      (funcall old-mode)))
+  (widen))
 
 (provide 'mode-html)
