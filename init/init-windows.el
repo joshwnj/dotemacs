2ec5093d9b97e1f28fbb51e731fc7f1c955bdf6e
Josh Johnston <josh@x-team.com>
2012-04-07 13:11:30 +1000

    new keys and functions for resizing windows

--- init/init-windows.el
+++ init/init-windows.el
@@ -1,18 +1,34 @@
+
+;;;;
+;; resize window with specific width/height
+;; source: http://dse.livejournal.com/67732.html
+
+(defun set-window-width (width)
+  "Set the window's size to 80 (or prefix arg WIDTH) columns wide."
+  (interactive "P")
+  (enlarge-window (- (or width 80) (window-width)) 'horizontal))
+
+(defun set-window-height (height)
+  "Set the window's height to 80 (or prefix arg HEIGHT) columns wide."
+  (interactive "P")
+  (enlarge-window (- (or height 80) (window-height))))
+
+
 (defvar windowsize-mode-map (make-keymap) "windowsize-mode keymap.")
 
 ;;;;
 ;; horizontal
 
-(define-key windowsize-mode-map (kbd "z") 'shrink-window-horizontally)
+(define-key windowsize-mode-map (kbd "a") 'shrink-window-horizontally)
 
-(define-key windowsize-mode-map (kbd "a") 'enlarge-window-horizontally)
+(define-key windowsize-mode-map (kbd "q") 'enlarge-window-horizontally)
 
-(define-key windowsize-mode-map (kbd "M-z")
+(define-key windowsize-mode-map (kbd "M-a")
   (lambda ()
     (interactive)
     (shrink-window-horizontally 10)))
 
-(define-key windowsize-mode-map (kbd "M-a")
+(define-key windowsize-mode-map (kbd "M-q")
   (lambda ()
     (interactive)
     (enlarge-window-horizontally 10)))
@@ -20,16 +36,16 @@
 ;;;;
 ;; vertical
 
-(define-key windowsize-mode-map (kbd "x") 'shrink-window)
+(define-key windowsize-mode-map (kbd "s") 'shrink-window)
 
-(define-key windowsize-mode-map (kbd "s") 'enlarge-window)
+(define-key windowsize-mode-map (kbd "w") 'enlarge-window)
 
-(define-key windowsize-mode-map (kbd "M-x")
+(define-key windowsize-mode-map (kbd "M-s")
   (lambda ()
     (interactive)
     (shrink-window 10)))
 
-(define-key windowsize-mode-map (kbd "M-s")
+(define-key windowsize-mode-map (kbd "M-w")
   (lambda ()
     (interactive)
     (enlarge-window 10)))
@@ -37,7 +53,7 @@
 (define-minor-mode windowsize-mode
   "Toggle WindowSize mode."
   'windowsize-mode-map
-  :lighter    " Size"
+  :lighter " Size"
   :init-value nil
   :global t)
 
