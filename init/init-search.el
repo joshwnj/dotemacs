d615b5b8735a589fbb81a0ab2f9c1c699b12e32f
Josh Johnston <josh@x-team.com>
2012-06-18 07:37:40 +1000

    updated search keys

--- init/init-search.el
+++ init/init-search.el
@@ -4,12 +4,10 @@
 (setq grep-find-command
       "export GREP_OPTIONS=\"--exclude=*.svn*\"; find . -type f -print0 | xargs -0 grep -nH --color -e ")
 
-;; colors for occur-mode
-(require 'color-occur)
-
-(global-set-key (kbd "C-s") 'occur)
-(global-set-key (kbd "M-A-g") 'grep-find)
-(global-set-key (kbd "M-A-,") 'previous-error)
-(global-set-key (kbd "M-A-.") 'next-error)
+(global-set-key (kbd "C-c o") 'occur)
+(global-set-key (kbd "C-c g") 'grep-find)
+(global-set-key (kbd "C-s") 'isearch-forward)
+(global-set-key (kbd "C-c ,") 'previous-error)
+(global-set-key (kbd "C-c .") 'next-error)
 
 (provide 'init-search)
