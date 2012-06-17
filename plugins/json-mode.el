d74394960f78a1b09970f914c03e055fbd342733
Josh Johnston <josh@x-team.com>
2012-06-18 07:41:30 +1000

    updated with beautify command

--- plugins/json-mode.el
+++ plugins/json-mode.el
@@ -22,4 +22,15 @@
   "Major mode for editing JSON files"
   (set (make-local-variable 'font-lock-defaults) '(json-font-lock-keywords-1 t)))
 
+;; from http://stackoverflow.com/a/7934783/495995
+
+(defun beautify-json ()
+  (interactive)
+  (let ((b (if mark-active (min (point) (mark)) (point-min)))
+        (e (if mark-active (max (point) (mark)) (point-max))))
+    (shell-command-on-region b e
+     "python -mjson.tool" (current-buffer) t)))
+
+(define-key json-mode-map (kbd "C-c C-f") 'beautify-json)
+
 (provide 'json-mode)
