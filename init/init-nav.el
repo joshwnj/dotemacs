d5b30b094ff695811dc6425d5f335b753fdb758f
Josh Johnston <josh@x-team.com>
2012-06-18 07:39:53 +1000

    ace-jump and keybinding for popping to mark + removed old whitespace file, it's been merged into another

--- init/init-nav.el
+++ init/init-nav.el
@@ -1,5 +1,13 @@
 ;;;;
+;; ace-jump
+
+(require 'ace-jump-mode)
+(global-set-key (kbd "C-c SPC") 'ace-jump-mode)
+(global-set-key (kbd "C-c l") 'ace-jump-line-mode)
+
+;;;;
 ;; Use "%" to jump to the matching parenthesis.
+
 (defun goto-match-paren (arg)
   "Go to the matching parenthesis if on parenthesis, otherwise insert
 the character typed."
@@ -7,6 +15,14 @@ the character typed."
   (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
     ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
     (t                    (self-insert-command (or arg 1))) ))
-(global-set-key (kbd "M-?") `goto-match-paren)
+(global-set-key (kbd "%") `goto-match-paren)
+
+;;;;
+;; Pop to mark
+
+(global-set-key (kbd "C-c b") 'pop-to-mark-command)
+(global-set-key (kbd "C-c C-b") 'pop-to-mark-command)
+
+;;;;
 
 (provide 'init-nav)
