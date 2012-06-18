93ebfdfe3191fdf927bdd5d0638d7b84e52049c8
Josh Johnston <josh@x-team.com>
2012-06-18 10:03:13 +1000

    smex setup

--- init/init-smex.el
+++ init/init-smex.el
@@ -1,19 +1,25 @@
 ;; initialize smex
 (require 'smex)
-(global-set-key [(meta x)] (lambda ()
-                             (interactive)
-                             (or (boundp 'smex-cache)
-                                 (smex-initialize))
-                             (global-set-key [(meta x)] 'smex)
-                             (smex)))
 
-(global-set-key [(shift meta x)] (lambda ()
-                                   (interactive)
-                                   (or (boundp 'smex-cache)
-                                       (smex-initialize))
-                                   (global-set-key [(shift meta x)] 'smex-major-mode-commands)
-                                   (smex-major-mode-commands)))
+(defun run-smex ()
+  "Run the smex command."
+  (interactive)
+  (or (boundp 'smex-cache)
+      (smex-initialize))
+  (global-set-key (kbd "M-x") 'smex)
+  (smex))
 
-(global-set-key (kbd "A-<return>") 'smex)
+(defun run-smex-major-mode ()
+  "Run the smex command using major mode commands."
+  (interactive)
+  (or (boundp 'smex-cache)
+      (smex-initialize))
+  (global-set-key (kbd "M-S-x") 'smex-major-mode-commands)
+  (smex-major-mode-commands))
+
+(global-set-key (kbd "M-x") 'run-smex-major-mode)
+(global-set-key (kbd "C-z") 'run-smex)
+
+;; ----
 
 (provide 'init-smex)
