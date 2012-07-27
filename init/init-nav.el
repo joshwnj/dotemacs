174d5c8133332369e2ab015fb8a14d2351f35a4b
Josh Johnston <josh@x-team.com>
2012-07-27 13:30:02 +1000

    experimental jump-mode for jumping to next/previous matching character

--- init/init-nav.el
+++ init/init-nav.el
@@ -1,9 +1,56 @@
 ;;;;
-;; ace-jump
+;; Jump mode: jump to the next/previous matching character
 
-(require 'ace-jump-mode)
-(global-set-key (kbd "C-c SPC") 'ace-jump-mode)
-(global-set-key (kbd "C-c l") 'ace-jump-line-mode)
+(defvar jump-mode/last-char
+  "Remember the last character we jumped to"
+  nil)
+
+(defun jump-mode/repeat-foward ()
+  "Jump forward, using the same chatacter as last time."
+  (interactive)
+  (search-forward (char-to-string jump-mode/last-char)))  
+
+(defun jump-mode/repeat-backward ()
+  "Jump backward, using the same chatacter as last time."
+  (interactive)
+  (search-backward (char-to-string jump-mode/last-char)))
+
+(defun jump-mode/on () 
+  "Turn on jump-mode"
+  (interactive)
+  (hl-line-mode 1)
+  (jump-mode 1))
+
+(defun jump-mode/off () 
+  "Turn off jump-mode"
+  (interactive)
+  (hl-line-mode 0)
+  (jump-mode 0))
+
+(defun jump-mode/start (char)
+  "Start jump mode"
+  (interactive "cForward to char: ")
+
+  ;; make search case-sensitive
+  (setq case-fold-search nil)
+
+  (jump-mode/on)
+  (setq jump-mode/last-char char)
+  (jump-mode/repeat-foward))
+
+(defvar jump-mode-map (make-keymap) "jump-mode keymap.")
+
+(define-key jump-mode-map (kbd "j") 'jump-mode/repeat-foward)
+(define-key jump-mode-map (kbd "k") 'jump-mode/repeat-backward)
+(define-key jump-mode-map (kbd "RET") 'jump-mode/off)
+
+(define-minor-mode jump-mode
+  "Toggle Jump mode."
+  'browse-mode-map
+  :lighter " Jump"
+  :init-value nil)
+
+(global-set-key (kbd "C-c j") 'jump-mode/start)
 
 ;;;;
 ;; Use "%" to jump to the matching parenthesis.
