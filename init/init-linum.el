085aa28017e1cf0b9402fa8eb80cc092fe0875f0
Josh Johnston <josh@x-team.com>
2012-08-08 17:00:50 +1000

    updated with help from @magnars: if you exit in the middle it hides line numbers again

--- init/init-linum.el
+++ init/init-linum.el
@@ -1,17 +1,23 @@
 ;; turn line numbers off by default
 (global-linum-mode -1)
 
+;;;;
+;; thanks to @magnars for help here
+
 (defun goto-line-with-feedback (&optional line)
   "Show line numbers temporarily, while prompting for the line number input"
   (interactive "P")
   (if line
-    (goto-line line)
-    (progn
-      (linum-mode 1)
-      (let ((line (read-number "Line: ")))
-        (goto-line line))
+      (goto-line line)
+    (unwind-protect
+        (progn
+          (linum-mode 1)
+          (goto-line (read-number "Goto line: ")))
       (linum-mode -1))))
 
 (global-set-key (kbd "C-l") 'goto-line-with-feedback)
+
+;; replace all goto-line
+(global-set-key (vector 'remap 'goto-line) 'goto-line-with-feedback)
 
 (provide 'init-linum)
