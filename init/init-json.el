d6ca2c6c53c1de97c7770d57ff6c1201d28e3e10
Josh Johnston <josh@x-team.com>
2012-09-17 13:30:30 +1000

    point to the correct json-mode

--- init/init-json.el
+++ init/init-json.el
@@ -1,7 +1,7 @@
 ;;;;
 ;; json
 
-(autoload 'json-mode "json-mode.el"
+(autoload 'json-mode "json-mode/json-mode.el"
    "Major mode for editing JSON files" t)
 
 (add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
