96f3ba3264cbf75851f859bf01b0efc9b888bcfb
Josh Johnston <josh@x-team.com>
2012-09-17 13:30:16 +1000

    nodejs mode

--- init/init-js.el
+++ init/init-js.el
@@ -3,6 +3,9 @@
 
 (add-to-list 'auto-mode-alist '("\\.js$" . javascript-mode))
 
+(add-to-list 'load-path "~/.emacs.d/plugins/nodejs-mode")
+(require 'nodejs-mode)
+
 ;; NOTE: expects jslint command to be in your path
 ;; npm install -g jslint
 
