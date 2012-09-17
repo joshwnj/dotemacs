bc019be118d0bb79e894fc46627c0d3e51797ec4
Josh Johnston <josh@x-team.com>
2012-09-17 13:20:33 +1000

    using git repo version of json-mode

--- plugins/json-mode/json-mode.el
+++ plugins/json-mode/json-mode.el
@@ -1,12 +1,15 @@
+;;; json-mode.el --- major mode for editing JSON files
+;;; - extends javascript mode to add better syntax highlighting
+;;; https://github.com/joshwnj/json-mode
+
 ;;;;
-;; json syntax highlighting
-;; https://github.com/joshwnj/json-mode
+;; syntax highlighting
 
 (defvar json-mode-hook nil)
 
 (defconst json-quoted-key-re "\\(\"[^\"]+?\"[ ]*:\\)")
 (defconst json-quoted-string-re "\\(\".*?\"\\)") 
-(defconst json-int-re "\\([^\"][0-9]+[^\"]\\)")
+(defconst json-number-re "[^\"]\\([0-9]+\\(\\.[0-9]+\\)?\\)[^\"]")
 (defconst json-keyword-re "\\(true\\|false\\|null\\)")
 
 (defconst json-font-lock-keywords-1
@@ -14,22 +17,22 @@
    (list json-quoted-key-re 1 font-lock-keyword-face)
    (list json-quoted-string-re 1 font-lock-string-face)
    (list json-keyword-re 1 font-lock-constant-face)
-   (list json-int-re 1 font-lock-constant-face)
+   (list json-number-re 1 font-lock-constant-face)
    )
   "Level one font lock.")
-
-(define-derived-mode json-mode javascript-mode "JSON"
-  "Major mode for editing JSON files"
-  (set (make-local-variable 'font-lock-defaults) '(json-font-lock-keywords-1 t)))
-
-;; from http://stackoverflow.com/a/7934783/495995
 
 (defun beautify-json ()
   (interactive)
   (let ((b (if mark-active (min (point) (mark)) (point-min)))
         (e (if mark-active (max (point) (mark)) (point-max))))
+
+;    (get-buffer-create "*json errors*")
     (shell-command-on-region b e
-     "python -mjson.tool" (current-buffer) t)))
+     "python -mjson.tool" (current-buffer)))); nil "*json errors*" t)))
+
+(define-derived-mode json-mode javascript-mode "JSON"
+  "Major mode for editing JSON files"
+  (set (make-local-variable 'font-lock-defaults) '(json-font-lock-keywords-1 t)))
 
 (define-key json-mode-map (kbd "C-c C-f") 'beautify-json)
 
