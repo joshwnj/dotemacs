2b9f213832a0b15ec71a5e7713c19fda7cd3085d
Josh Johnston <josh@x-team.com>
2012-09-17 13:22:30 +1000

    updated autosave config

--- init/init-backups.el
+++ init/init-backups.el
@@ -1,8 +1,15 @@
 ;;;;
 ;; backups
 
+;; source: http://snarfed.org/gnu_emacs_backup_files
+;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
 (custom-set-variables
+  '(auto-save-file-name-transforms '((".*" "~/.emacs_autosaves/\\1" t)))
   '(backup-directory-alist '((".*" . "~/.emacs_backups/"))))
+
+;; create the dirs if necessary, since emacs won't
+(make-directory "~/.emacs_autosaves/" t)
+(make-directory "~/.emacs_backups/" t)
 
 ;; create the directory
 (make-directory "~/.emacs_backups/" t)
