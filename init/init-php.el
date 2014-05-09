d4ad12f1930c0cf47c9895833cd715bcd9acaf82
Josh Johnston <josh@x-team.com>
2014-05-09 14:05:54 +1000

    updated php config

--- init/init-php.el
+++ init/init-php.el
@@ -18,29 +18,10 @@
                            (c-set-offset 'arglist-intro 'ywb-php-lineup-arglist-intro)
                            (c-set-offset 'arglist-close 'ywb-php-lineup-arglist-close)))
 
-;;;;
-;; flymake
 
-(defun flymake-php-init ()
-  "Use php to check the syntax of the current file."
-  (let* ((temp (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace))
-         (local (file-relative-name temp (file-name-directory buffer-file-name))))
-    (list "php" (list "-f" local "-l"))))
-
-(add-to-list 'flymake-err-line-patterns
-             '("\\(Parse\\|Fatal\\) error: +\\(.*?\\) in \\(.*?\\) on line \\([0-9]+\\)$" 3 4 nil 2))
-
-(add-to-list 'flymake-allowed-file-name-masks '("\\.php$" flymake-php-init))
-
-;; extra extensions
-(add-to-list 'flymake-allowed-file-name-masks '("\\.inc$" flymake-php-init))
-(add-to-list 'flymake-allowed-file-name-masks '("\\.class$" flymake-php-init))
-
-;; auto-start flymake
-(add-hook 'php-mode-hook
-          (lambda ()
-            (define-key php-mode-map (kbd "C-c .") 'flymake-goto-next-error)
-            (flymake-mode 1)))
+(defun my-php-hook-function ()
+  (set (make-local-variable 'compile-command) (format "phpcs --report=emacs --standard=PEAR %s" (buffer-file-name))))
+(add-hook 'php-mode-hook 'my-php-hook-function)
 
 ;; auto-start eldoc mode
 (add-hook 'php-mode-hook 'turn-on-eldoc-mode)
