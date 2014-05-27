38f3a4ade8cde872e16d7f1b605f1ecb06fa4bf5
Josh Johnston <josh@x-team.com>
2014-05-28 08:09:33 +1000

    flycheck instead of flymake

--- init/init-js.el
+++ init/init-js.el
@@ -1,28 +1,6 @@
-;; source: http://www.emacswiki.org/emacs-fr/FlymakeJavaScript
-;; source: http://lapin-bleu.net/riviera/?p=191
-
-;; NOTE: expects jslint command to be in your path
-;; npm install -g jslint
-
-(when (load "flymake" t)
-  (defun flymake-jslint-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-		       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list "jslint" (list "--terse" local-file))))
-
-  (setq flymake-err-line-patterns
-	(cons '("^\\(.*\\)(\\([[:digit:]]+\\)):\\(.*\\)$"
-		1 2 nil 3)
-	      flymake-err-line-patterns))
-
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.js\\'" flymake-jslint-init)))
-
+(require 'flycheck)
 (add-hook 'js-mode-hook
-	  (lambda () (flymake-mode t)))
+          (lambda () (flycheck-mode t)))
 
 ;;;;
 ;; to install:
@@ -31,5 +9,7 @@
 (defun grasp-current-buffer (arg)
   (interactive "MGrasp: ")
   (compile (concat "grasp -H '" arg "' " (buffer-file-name) " | strip-ansi")))
+
+;;;;
 
 (provide 'init-js)
