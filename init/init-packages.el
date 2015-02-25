390c1041660c2d7c4bfec21cd568228c4193ac47
Josh Johnston <josh@x-team.com>
2015-02-25 19:31:11 +1100

    disabled some packages

--- init/init-packages.el
+++ init/init-packages.el
@@ -1,59 +1,82 @@
 (require 'package)
 
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+                          ("gnu"  . "http://elpa.gnu.org/packages/")
+                                        ;("marmalade" . "http://marmalade-repo.org/packages/")
+                          ))
 
 (package-initialize)
 
 (defvar required-packages
   '(
-    ;; ----
-    ;; base
+     ;; ----
+     ;; base
 
-    ace-window
-    change-inner
-    company
-    dropdown-list
-    electric-case
-    expand-region
-    find-things-fast
-    flycheck
-    goto-last-change
-    hiwin
-    mark
-    move-text
-    projectile
-    relative-line-numbers
-    rotate
-    shell-pop
-    simpleclip
-    smex
-    tidy
-    undo-tree
-    use-package
-    visual-regexp
-    whole-line-or-region
-    wrap-region
-    yascroll
+     ace-window
+     ag
+     change-inner
+     company
+     company-tern
+    ;; dired+
+    ;; dropdown-list
+    ;; electric-case
+     expand-region
+    ;; find-things-fast
+     ;; flycheck
+    ;; flycheck-clojure
+    ;; golden-ratio
+     goto-last-change
+     ;; hiwin ; disabled: seems to cause incorrect buffer display when opening a file in emacsclient
+    ;; ;mark
+    ;; move-text
+    ;; ;occur-default-current-word
+    ;; paredit
+     peep-dired
+     projectile
+    ;; rainbow-delimiters
+     relative-line-numbers
+     rotate
+     shell-pop
+    ;; show-marks
+     simpleclip
+     smex
+    ;; tidy
+     undo-tree
+     use-package
+    ;; visual-regexp
+    ;; whole-line-or-region
+    ;; wrap-region
 
     ;; ----
     ;; languages
 
-    apache-mode
-    erlang
-    json-mode
-    less-css-mode
-    markdown-mode
-    php-extras
-    php-mode
+     apache-mode
+     clojure-mode
+     erlang
+     json-mode
+     less-css-mode
+     markdown-mode
+     ;; php-extras
+     php-mode
+     scss-mode
+     web-mode
 
     ;; ----
     ;; utilities
 
-    circe
-    magit
-    zencoding-mode
+     cider
+     editorconfig
+    ;; erc-colorize
+    ;; erc-hl-nicks
+     magit
+    ;; zencoding-mode
+
+    ;; ----
+    ;; trial
+
+     ;; auto-highlight-symbol
+     ;; ido-vertical-mode
+     ;; volatile-highlights
     ) "A list of packages we want to install.")
 
 (defun install-required-packages ()
